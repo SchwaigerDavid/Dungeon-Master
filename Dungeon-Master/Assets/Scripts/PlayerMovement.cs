fc14cdfce93803ec5ad213b5dc16ee86095314e9
Dave <ds@mscg.at>
2020-03-15 14:52:13 +0100

    Player Movement
    Player Movement implemented

---
+++ Dungeon-Master/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public Vector2 pos;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+        pos.x += Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        pos.y += Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        transform.position = pos; 
+    }
+}
