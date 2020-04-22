c67e5b8f2a50ad156e987c5ffa53148071a2ddc4
Sebastian Moser <sebastian.moser11@gmx.at>
2020-04-22 23:30:48 +0200

    Room1, RandomObject teilweise fertig

---
+++ Dungeon-Master/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour      //Script in BETA, noch nicht funktionsfähig!!!!!
+{                                               //Tutorial: https://youtu.be/WGn1zvLSndk
+    public GameObject[] objekte;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    int randEnemy;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        StartCoroutine( waitSpawner());
+    }
+
+    IEnumerator waitSpawner()
+    {
+    while (true)
+    {
+        randObject= Random.Range (0, objekte.length);
+
+        Vector3 spawnPosition= new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), Random.Range (-spawnValues.y, spawnValues.y));
+
+        Instantiate (objekte[randObject], spawnPosition + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation);
+
+    }
+}
+}
