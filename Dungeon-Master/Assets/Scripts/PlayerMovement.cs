745415547e4aaa88e1662f355e1d207668306b34
VoglbauerPatrick <51457936+VoglbauerPatrick@users.noreply.github.com>
2020-05-05 14:54:40 +0200

    Interaktion
    Spielmechaniken verbessert und interaktionen

--- Dungeon-Master/Assets/Scripts/PlayerMovement.cs
+++ Dungeon-Master/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed;
+    public float speed;
     public Vector2 pos;
+    public Rigidbody2D body;
+
+    private float x, y;
   
     // Start is called before the first frame update
     void Start()
@@ -16,9 +19,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pos = transform.position;
-        pos.x += Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-        pos.y += Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-        transform.position = pos; 
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        var moveVector = new Vector3(x, y, 0);
+
+        body.MovePosition(new Vector2((transform.position.x + moveVector.x * speed * Time.deltaTime),
+            transform.position.y + moveVector.y * speed * Time.deltaTime));
     }
 }
