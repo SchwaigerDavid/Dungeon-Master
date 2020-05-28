fb27c5f59a4ff03046c1ae1c1d577df6e5b4a72e
Dave <ds@mscg.at>
2020-05-28 11:05:11 +0200

    Rätsel Fertig
    Jetzt kann man zu den Vasen gehen mit e erscheint ein Canvas das anzeigt welchen code du eingeben musst dann gehst du zum rechteck drückst e und gibst den code ein
    wenn der code richtig ist wird dein bildschirm grün wenn er falsch ist wird er rot.
    Ich habe die interaction über arbeitet da sie nicht immer funktioniert hat.

--- Dungeon-Master/Assets/Scripts/Interaction.cs
+++ Dungeon-Master/Assets/Scripts/Interaction.cs
@@ -1,35 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Interaction : MonoBehaviour
 {
 
-    public GameObject text;
+    public TextMeshProUGUI text;
+    public Canvas texton;
+    public TextMeshProUGUI Title; 
 
+
+    public string title;
     public string code;
 
     private bool enter = false;
 
-    private void OnCollisionStay2D(Collision2D collision)
+    /*private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player" && Input.GetButton("Fire1"))
+        if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.E))
         {
             Debug.Log("pressed");
 
             if (gameObject.tag == "Rätsel")
             {
-                text.SetActive(true);
+                texton.SetActive(true);
+                text.text = code;
+
             }
-            else if(gameObject.tag == "Enter")
+            else if (gameObject.tag == "Enter")
             {
                 Debug.Log("ent");
                 enter = true;
             }
         }
+    }*/
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Enter");
+        if (collision.gameObject.tag == "Player")
+        {
+            enter = true;
+        }
+        else {
+            enter = false;
+        }
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Exit");
+        if (collision.gameObject.tag=="Player") {
+            enter = false;
+            texton.enabled=false;
+        }
+    }
+
+
 
     void Update()
     {
+        if (enter == true && Input.GetKey(KeyCode.E)) {
+            texton.enabled=true;
+            Title.text = title; 
+            text.text = code;
+        }
     }
 }
