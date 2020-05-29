f18495afe3caa0e5e2f8bd9bbb9648dc1c61e85a
Dave <ds@mscg.at>
2020-05-29 12:40:54 +0200

    Playerin three directions
    I made a player myself so please write in the whatsapp group which one you want to use

--- Dungeon-Master/Assets/Scripts/Doortransport.cs
+++ Dungeon-Master/Assets/Scripts/Doortransport.cs
@@ -6,12 +6,10 @@ using UnityEngine.SceneManagement;
 public class Doortransport : MonoBehaviour
 {
     public string nextLevel;
-    public string thisLevel;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag.Equals("Player")) {
             SceneManager.LoadScene(nextLevel);
-            SceneManager.UnloadScene(thisLevel);
         }
     }
 }
