d6e71f73dd56a8ab9ba92b4c1f26c5e0fa22842d
Sebastian Moser <sebastian.moser11@gmx.at>
2020-06-14 19:28:12 +0200

    Pause Menü

---
+++ Dungeon-Master/Assets/Scripts/AbfrageL2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class AbfrageL2 : MonoBehaviour
+{
+    public int count;
+    public Animator door;
+     bool open = false;
+    float timer=60.0f;
+    public TextMeshProUGUI time; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        time.text = timer+"s";
+        if (Druckplatte.druckcount == count)
+        {
+            door.GetComponent<Animator>();
+            door.SetBool("Open", true);
+            open = true;
+            Druckplatte.druckcount = 0;
+        }
+        if (timer <= 0.0f && Druckplatte.druckcount != count) {
+            SceneManager.UnloadScene("Level2");
+            SceneManager.LoadScene("Level2");
+            Debug.Log("Out Of time");
+        }
+
+    }
+}
---
+++ Dungeon-Master/Assets/Scripts/Doortransport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Doortransport : MonoBehaviour
+{
+    public GameObject blackScreen;
+    public Animator blackScreenAnim;
+    public string nextLevel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player")) {
+
+            StartCoroutine("newRoom");
+        }
+    }
+
+    IEnumerator newRoom()
+    {
+        blackScreen.SetActive(true);
+        blackScreenAnim.SetTrigger("leave");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(nextLevel);
+    }
+}
